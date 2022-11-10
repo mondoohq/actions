f386e7ea392c100cb596ee8ae7a799b0b76c41cb
Christian Zunker <christian@mondoo.com>
2022-11-10 10:19:59 +0100

    Simplify script
    Signed-off-by: Christian Zunker <christian@mondoo.com>

--- docker-image/scripts/entrypoint.sh
+++ docker-image/scripts/entrypoint.sh
@@ -7,17 +7,8 @@ set -eu
 # But when they are not specified, they are passed as an empty string
 # This results in an error when passed to cnspec, because it only expects one argument
 # Because of that: If the last argument is an empty string, remove it
-numArgs=$#
-for last_argument in "$@"; do
-  true
-done
-if [ "${last_argument}" = "" ]; then
-  echo "Removing last argument, because it is empty"
-  echo "before:" "${@}"
-  indexSequence=$(seq -s',' 1 ${numArgs})
-  set -- $(echo "${@}" | cut -d' ' -f ${indexSequence})
-  echo "after:" "${@}"
-fi
+# This will automatically take care of it:
+set -- $(echo "${@}")
 
 # Run the action
 cnspec "$@"
