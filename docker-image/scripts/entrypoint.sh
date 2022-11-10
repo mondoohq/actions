afcfcba8ec2a1b4e5b0e34a7e384b62051214d38
Christian Zunker <christian@mondoo.com>
2022-11-10 10:14:06 +0100

    Re-write to sh becasue of alpine image
    Signed-off-by: Christian Zunker <christian@mondoo.com>

--- docker-image/scripts/entrypoint.sh
+++ docker-image/scripts/entrypoint.sh
@@ -1,15 +1,21 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-set -Eeuo pipefail
+set -eu
+
 
 # We offer to specify optional args to the action
 # But when they are not specified, they are passed as an empty string
 # This results in an error when passed to cnspec, because it only expects one argument
 # Because of that: If the last argument is an empty string, remove it
-if [[ "${@: -1}" == "" ]]; then
+numArgs=$#
+for last_argument in "$@"; do
+  true
+done
+if [ "${last_argument}" = "" ]; then
   echo "Removing last argument, because it is empty"
   echo "before:" "${@}"
-  set -- "${@:1:$(($#-1))}"
+  indexSequence=$(seq -s',' 1 ${numArgs})
+  set -- $(echo "${@}" | cut -d' ' -f ${indexSequence})
   echo "after:" "${@}"
 fi
 
